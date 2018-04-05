e4235933d3c99d5322e414461b9cac338466684b
Fabio Pitino <pitinofabio@gmail.com>
2018-04-05 20:27:21 +0100

    refactoring Subscribe into Connect for better connection handling and better API

--- airtime/client.go
+++ airtime/client.go
@@ -9,27 +9,56 @@ import (
 
 // Client struct
 type Client struct {
-	Host string
+	Host   string
+	socket *websocket.Conn
 }
 
-// Subscribe to a feed with a strategy function that handles the message
-func (c *Client) Subscribe(feed string, strategy func(msg []byte)) error {
+// Connection handled back to the user
+type Connection struct {
+	Inbox  chan []byte
+	Errors chan error
+	Closed chan bool
+}
+
+// Connect to a feed in order to establish a connection
+func (c *Client) Connect(feed string) (*Connection, error) {
 	conn, _, err := websocket.DefaultDialer.Dial(c.subscribeURL(feed), nil)
 	if err != nil {
-		return fmt.Errorf("could not subscribe to feed '%s': %v", feed, err)
+		return nil, fmt.Errorf("could not subscribe to feed '%s': %v", feed, err)
 	}
-	defer conn.Close()
+	c.socket = conn
 
-	for {
-		_, message, err := conn.ReadMessage()
-		if err != nil {
-			return fmt.Errorf("could not read message: %v", err)
-		}
-		strategy(message)
+	connection := &Connection{
+		Inbox:  make(chan []byte),
+		Errors: make(chan error),
+		Closed: make(chan bool, 1),
 	}
-	// conn.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
-	// return nil
+
+	go c.loop(connection)
+
+	return connection, nil
 }
+
+// Close the connection currently opened for the client
+func (c *Client) Close() error {
+	return c.socket.Close()
+}
+
+func (c *Client) loop(conn *Connection) {
+	for {
+		_, message, err := c.socket.ReadMessage()
+
+		if err != nil {
+			conn.Errors <- err
+			continue
+		}
+
+		conn.Inbox <- message
+	}
+}
+
+// conn.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+// return nil
 
 func (c *Client) subscribeURL(feed string) string {
 	path := fmt.Sprintf("/feeds/%s/subscribe", feed)
--- cmd/subscriber.go
+++ cmd/subscriber.go
@@ -9,27 +9,18 @@ import (
 func main() {
 	client := &airtime.Client{Host: "localhost:8080"}
 
-	// TODO: use the following design instead
-	// conn, err := client.Connect("test")
-	// if err != nil {
-	//     log.Fatal(err)
-	// }
-	// for {
-	//     select {
-	//     case msg := <-conn.Inbox:
-	//         log.Printf("received message: %s", m)
-	//     case err := <-conn.Errors:
-	//         log.Printf("error: %v", err)
-	//     case <-conn.Closed:
-	//         log.Println("connection closed")
-	//     }
-	// }
-
-	err := client.Subscribe("test", func(m []byte) {
-		log.Printf("received message: %s", m)
-	})
+	conn, err := client.Connect("test")
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Println("connection closed")
+	for {
+		select {
+		case msg := <-conn.Inbox:
+			log.Printf("received msg: %s", msg)
+		case err := <-conn.Errors:
+			log.Printf("error: %v", err)
+		case <-conn.Closed:
+			log.Println("connection closed")
+		}
+	}
 }
